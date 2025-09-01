38e53fd177fe96e0d09ffb3a3af6bc10e886ebfa
Perekhristova <nas.prhv@gmail.com>
2025-09-01 18:45:08 +0700

    Проверка 3

--- src/main.js
+++ src/main.js
@@ -10,7 +10,8 @@ import {initTable} from "./components/table.js";
 // @todo: подключение
 import {initPagination} from './components/pagination.js';
 import {initSorting} from './components/sorting.js';
-
+import {initFiltering} from './components/filtering.js';
+import {initSearching} from './components/searching.js';
 
 // Исходные данные используемые в render()
 const {data, ...indexes} = initData(sourceData);
